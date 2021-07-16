5a238d79f6314d90f9269bbc1fb08862ef7b50d2
pborio <pauloborio@gmail.com>
2021-07-16 21:18:31 +0200

    creating database shared multitenant

--- src/main/java/com/borio/authorization/config/multinenancy/client/per/database/exceptions/TenantNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.borio.authorization.config.multinenancy.client.per.database.exceptions;
-
-import com.borio.authorization.domain.exceptions.GeneralAuthorizationException;
-
-public class TenantNotFoundException extends GeneralAuthorizationException {
-    public TenantNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/borio/authorization/config/multinenancy/client/per/database/services/EncryptionService.java
+++
@@ -1,8 +0,0 @@
-package com.borio.authorization.config.multinenancy.client.per.database.services;
-
-public interface EncryptionService {
-
-    String encrypt(String strToEncrypt, String secret, String salt);
-    String decrypt(String strToDecrypt, String secret, String salt);
-
-}
